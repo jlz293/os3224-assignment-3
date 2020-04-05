12a57c9aa18ec4168ba4dd533e8ec68e81fd05ba
jlz293 <35945503+jlz293@users.noreply.github.com>
2020-04-05 20:38:58 +0200

    Update defs.h

--- defs.h
+++ defs.h
@@ -118,6 +118,7 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
+void            wait_stat(int *wtime, int *rtime, int *iotime, int *status);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
